6d846808d4738f2551814013325c73d9e8d2d3bc
ismartynenko <martein87@gmail.com>
2022-03-30 17:21:36 +0300

    Shortener v0.2

--- shortener/database.py
+++ shortener/database.py
@@ -8,6 +8,7 @@ class Sql:
         self.db_name = dbname
 
     def __enter__(self):
+        # Создаем базу данных
         self.conn = sqlite3.connect(self.db_name)
         cur = self.conn.cursor()
         cur.execute("CREATE TABLE IF NOT EXISTS Urls"
@@ -23,28 +24,42 @@ class Sql:
         self.conn.close()
 
     def post(self, path: str):
+        # Генерируем короткую ссылку с помощью модуля hashlib
         hash_object = hashlib.sha512(path.encode())
         slug = hash_object.hexdigest()[0:8]
         cur = self.conn.cursor()
+        # Добавляем запись в БД вида - Полный URL | Короткая ссылка | Время создания
         try:
             cur.execute("INSERT INTO Urls (Uri, Slug, Ctime) VALUES (:uri, :slug, :ctime)",
                         {'uri': path, 'slug': slug, 'ctime': time()})
+        # Если ссылка уже имеется в БД, обновляем для нее время создания...
         except sqlite3.IntegrityError:
+            cur.execute("UPDATE Urls"
+                        " SET Ctime = :ctime"
+                        " WHERE Uri = :uri",
+                        {'ctime': time(), 'uri': path})
+            self.conn.commit()
+            # ...и возвращаем короткую ссылку
             cur.execute("SELECT Slug FROM Urls WHERE Uri = :uri",
                         {'uri': path})
             return cur.fetchone()[0]
         self.conn.commit()
+        # Возвращаем короткую ссылку
         return slug
 
     def get(self, path: str):
+        # Поиск в БД полной ссылки
         cur = self.conn.cursor()
         cur.execute("SELECT Uri FROM Urls WHERE Slug = :path",
                     {'path': path.lstrip('/')})
+        # Возвращаем результат поиска
         return cur.fetchone()
 
+    # Проверка и чистка записей с истекшим временем хранения
     def clean(self, lifetime: int):
         cur = self.conn.cursor()
         cur.execute("DELETE FROM Urls WHERE Ctime < :time",
                     {'time': time() - lifetime})
         self.conn.commit()
+        # Возвращаем кол-во удаленных записей
         return cur.rowcount
