9338e33450f3d4840bd7b3cc5534c3f457107ee8
ismartynenko <martein87@gmail.com>
2022-04-07 21:25:54 +0300

    Shortener v0.5

--- shortener/server.py
+++ shortener/server.py
@@ -31,7 +31,7 @@ class MyHandler(BaseHTTPRequestHandler):
         if code == 200:
             info = '<a href="./' + slug + '">' + link + '</a>'
             txt = html['200'].replace('_INFO', info)
-            self.wfile.write(txt.encode()) if self.ua != 'curl' else self.wfile.write(link.encode())
+            self.wfile.write(txt.encode()) if self.ua != 'curl' else self.wfile.write(('http://'+link).encode())
         elif code == 301:
             txt = html['301'].replace('_INFO', link)
             self.wfile.write(txt.encode()) if self.ua != 'curl' else self.wfile.write(link.encode())
